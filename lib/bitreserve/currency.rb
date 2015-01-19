d7f8c6b314a1fd0e13781b2e4f25dfaa0ff43808
mikong <michaelgalero@gmail.com>
2015-01-19 17:14:38 +0800

    Expose currencies.

--- lib/bitreserve/currency.rb
+++ lib/bitreserve/currency.rb
@@ -1,13 +1,17 @@
 module Bitreserve
 
-    CURRENCIES = {
-      btc: "BTC",
-      cny: "CNY",
-      eur: "EUR",
-      gbp: "GBP",
-      jpy: "JPY",
-      usd: "USD",
-      xau: "XAU"
-    }
+  CURRENCIES = {
+    btc: "BTC",
+    cny: "CNY",
+    eur: "EUR",
+    gbp: "GBP",
+    jpy: "JPY",
+    usd: "USD",
+    xau: "XAU"
+  }
+
+  def self.currencies
+    CURRENCIES.values
+  end
 
 end
